2c2aab9a53ef62bc392f59325c6f1c742611103a
ViniciusGenka <vini.genka@hotmail.com>
2022-07-16 14:55:26 -0300

    Connection to mongodb database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to mongodb database"));
 
 const usersRoutes = require("./routes/usersRoutes");
 app.use("/users", usersRoutes);
